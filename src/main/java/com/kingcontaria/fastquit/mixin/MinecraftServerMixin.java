09c53ebbfbc9a89c270884321ac23c1f0750b2ad
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-11-13 17:49:21 +0100

    uncomment

--- src/main/java/com/kingcontaria/fastquit/mixin/MinecraftServerMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/MinecraftServerMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
 
-    //@Inject(method = "exit", at = @At("RETURN"))
+    @Inject(method = "exit", at = @At("RETURN"))
     private void fastQuit_finishSaving(CallbackInfo ci) {
         //noinspection ConstantConditions
         if ((Object) this instanceof IntegratedServer) {

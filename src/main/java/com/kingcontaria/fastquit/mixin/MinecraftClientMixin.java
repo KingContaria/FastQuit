56a2e354da97abf424c4d14f34b4f69cbff0b973
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-11-17 13:45:21 +0100

    fix toasts not rendering sometimes & simplify
    fix: toasts not rendering when clientside unloading takes longer than serverside
    simplified shutdown/crash logic

--- src/main/java/com/kingcontaria/fastquit/mixin/MinecraftClientMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/MinecraftClientMixin.java
@@ -4,12 +4,22 @@ import com.kingcontaria.fastquit.FastQuit;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.MessageScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.client.toast.ToastManager;
 import net.minecraft.server.integrated.IntegratedServer;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
@@ -17,43 +27,45 @@ public abstract class MinecraftClientMixin {
     @Shadow public abstract void setScreenAndRender(Screen screen);
     @Shadow protected abstract void render(boolean tick);
 
-    @Unique private boolean stopping;
+    @Shadow @Final private ToastManager toastManager;
+
+    @Unique private Set<IntegratedServer> cached;
 
     @Redirect(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;isStopping()Z"))
     private boolean fastQuit(IntegratedServer server) {
         FastQuit.savingWorlds.add(server);
+        this.cached = new HashSet<>(FastQuit.savingWorlds);
         return true;
+    }
+
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;clear()V", shift = At.Shift.AFTER))
+    private void fastQuit_reAddClearedToasts(Screen screen, CallbackInfo ci) {
+        if (this.cached != null) {
+            this.cached.removeAll(FastQuit.savingWorlds);
+            this.cached.forEach(server -> this.toastManager.add(new SystemToast(SystemToast.Type.TUTORIAL_HINT, Text.translatable("toast.fastquit.title"), Text.translatable("toast.fastquit.description", server.getSaveProperties().getLevelName()))));
+            this.cached = null;
+        }
     }
 
     // using MixinExtras' @WrapCondition would be perfect here, but it doubles the filesize
     // I doubt anyone else will redirect this call anyway, might reconsider in the future tho
     @Redirect(method = "reset", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;render(Z)V"))
     private void fastQuit_doNotOpenSaveScreen(MinecraftClient client, boolean tick, Screen screen) {
-        if (!(screen instanceof MessageScreen) || FastQuit.renderSavingScreen) {
+        if (FastQuit.renderSavingScreen || !(screen instanceof MessageScreen && screen.getTitle().equals(Text.translatable("menu.savingLevel")))) {
             this.render(tick);
         }
     }
 
-    @Inject(method = "scheduleStop", at = @At("HEAD"))
+    @Inject(method = "run", at = @At("RETURN"))
     private void fastQuit_waitForSaveOnShutdown(CallbackInfo ci) {
-        this.fastQuit_waitForSave();
-    }
-
-    @Inject(method = "cleanUpAfterCrash", at = @At(value = "INVOKE", target = "Ljava/lang/System;gc()V", ordinal = 1))
-    private void fastQuit_waitForSaveOnCrash(CallbackInfo ci) {
-        this.fastQuit_waitForSave();
+        if (!FastQuit.savingWorlds.isEmpty()) {
+            FastQuit.wait(FastQuit.savingWorlds);
+        }
     }
 
     @ModifyArg(method = "cleanUpAfterCrash", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;disconnect(Lnet/minecraft/client/gui/screen/Screen;)V"))
     private Screen fastQuit_renderSaveAndQuitScreenOnCrash(Screen screen) {
         this.setScreenAndRender(screen);
         return screen;
-    }
-
-    private void fastQuit_waitForSave() {
-        if (!this.stopping && !FastQuit.savingWorlds.isEmpty()) {
-            this.stopping = true;
-            FastQuit.wait(FastQuit.savingWorlds);
-        }
     }
 }

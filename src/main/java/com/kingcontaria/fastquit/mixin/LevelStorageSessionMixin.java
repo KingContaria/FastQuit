e8a754a26fcad76dcd9cd003ea966d7bb02e9f2a
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-11-18 20:39:15 +0100

    fix wrongfully suppressing log

--- src/main/java/com/kingcontaria/fastquit/mixin/LevelStorageSessionMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/LevelStorageSessionMixin.java
@@ -2,6 +2,8 @@ package com.kingcontaria.fastquit.mixin;
 
 import com.kingcontaria.fastquit.FastQuit;
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
@@ -25,8 +27,9 @@ public abstract class LevelStorageSessionMixin {
 
     @WrapWithCondition(method = "backupLevelDataFile(Lnet/minecraft/util/registry/DynamicRegistryManager;Lnet/minecraft/world/SaveProperties;Lnet/minecraft/nbt/NbtCompound;)V", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
     private boolean fastQuit_doNotLogErrorServerSide(Logger logger, String s, Object o1, Object o2) {
+        IntegratedServer server = MinecraftClient.getInstance().getServer();
         //noinspection ConstantConditions
-        return FastQuit.isSavingWorld((LevelStorage.Session) (Object) this);
+        return (server != null && !server.isStopped() && ((MinecraftServerAccessor) server).getSession() == (Object) this) || FastQuit.isSavingWorld((LevelStorage.Session) (Object) this);
     }
 
     @WrapWithCondition(method = "deleteSessionLock", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))

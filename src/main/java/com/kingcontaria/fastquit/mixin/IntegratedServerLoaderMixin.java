7a2013ebf07b52ac222cd065ef4de554c66a33ab
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-11-18 14:11:17 +0100

    simplify waiting for save on rejoin

--- src/main/java/com/kingcontaria/fastquit/mixin/IntegratedServerLoaderMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/IntegratedServerLoaderMixin.java
@@ -3,9 +3,7 @@ package com.kingcontaria.fastquit.mixin;
 import com.kingcontaria.fastquit.FastQuit;
 import net.minecraft.server.integrated.IntegratedServerLoader;
 import net.minecraft.world.level.storage.LevelStorage;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,13 +13,8 @@ import java.util.Collections;
 @Mixin(IntegratedServerLoader.class)
 public abstract class IntegratedServerLoaderMixin {
 
-    @Shadow @Nullable protected abstract LevelStorage.Session createSession(String levelName);
-
-    @Inject(method = "createSession", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false), cancellable = true)
+    @Inject(method = "createSession", at = @At("HEAD"))
     private void fastQuit_waitForSaveOnWorldLoad(String levelName, CallbackInfoReturnable<LevelStorage.Session> cir) {
-        FastQuit.getSavingWorld(levelName).ifPresent(server -> {
-            FastQuit.wait(Collections.singleton(server));
-            cir.setReturnValue(this.createSession(levelName));
-        });
+        FastQuit.getSavingWorld(levelName).ifPresent(server -> FastQuit.wait(Collections.singleton(server)));
     }
 }

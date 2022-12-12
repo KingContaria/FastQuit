c6e8e9d8539de7e339c7ac5fd52dc4d2f9564e8e
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-12-12 23:38:17 +0100

    1.17 backport part 3

--- src/main/java/com/kingcontaria/fastquit/mixin/LevelStorageMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/LevelStorageMixin.java
@@ -4,7 +4,7 @@ import com.kingcontaria.fastquit.FastQuit;
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import net.minecraft.world.level.storage.LevelStorage;
 import net.minecraft.world.level.storage.LevelSummary;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -31,7 +31,7 @@ public abstract class LevelStorageMixin {
         });
     }
 
-    @WrapWithCondition(method = "getLevelList", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
+    @WrapWithCondition(method = "getLevelList", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
     private boolean fastQuit_doNotLogWarning(Logger logger, String s, Object file, Object exception) {
         return !(file instanceof File) || FastQuit.getSavingWorld(((File) file).toPath()).isEmpty();
     }

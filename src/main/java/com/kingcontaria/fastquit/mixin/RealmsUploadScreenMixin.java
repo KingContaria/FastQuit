aefe6d04823415a90f0e95a14ff9299df966be1e
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-12-29 21:36:23 +0100

    possibly fixed a bug/crash
    there mightve been a bug/crash when uploading worlds to realms because i think rendering wouldve happened on another thread so i think it wouldnt go well, cant test that theory tho cuz i dont have realms lol
    anyway this moves it to render thread so that should be good now

--- src/main/java/com/kingcontaria/fastquit/mixin/RealmsUploadScreenMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/RealmsUploadScreenMixin.java
@@ -1,20 +1,31 @@
 package com.kingcontaria.fastquit.mixin;
 
 import com.kingcontaria.fastquit.FastQuit;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.realms.gui.screen.RealmsUploadScreen;
+import net.minecraft.text.Text;
+import net.minecraft.world.level.storage.LevelSummary;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
 import java.util.Collections;
+import java.util.Objects;
 
 @Mixin(RealmsUploadScreen.class)
-public abstract class RealmsUploadScreenMixin {
+public abstract class RealmsUploadScreenMixin extends Screen {
 
-    @Inject(method = "tarGzipArchive", at = @At("HEAD"))
-    private void fastQuit_waitForSaveOnRealmsUpload(File pathToDirectoryFile, CallbackInfoReturnable<File> cir) {
-        FastQuit.getSavingWorld(pathToDirectoryFile.toPath()).ifPresent(server -> FastQuit.wait(Collections.singleton(server)));
+    @Shadow @Final private LevelSummary selectedLevel;
+
+    protected RealmsUploadScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "upload", at = @At("HEAD"))
+    private void fastQuit_waitForSaveOnRealmsUpload(CallbackInfo ci) {
+        FastQuit.getSavingWorld(Objects.requireNonNull(this.client).getLevelStorage().getSavesDirectory().resolve(this.selectedLevel.getName())).ifPresent(server -> FastQuit.wait(Collections.singleton(server)));
     }
 }

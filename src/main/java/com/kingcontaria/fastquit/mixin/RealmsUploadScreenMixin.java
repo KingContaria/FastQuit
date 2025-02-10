98583a7d7477fbf5b36bb47e1cf7387595fb39df
Kel Cuprum <pavvvlovvva13@yandex.ru>
2025-02-10 19:59:08 +0300

    Update to 1.21.2(3) (#56)

--- src/main/java/com/kingcontaria/fastquit/mixin/RealmsUploadScreenMixin.java
+++ src/main/java/com/kingcontaria/fastquit/mixin/RealmsUploadScreenMixin.java
@@ -1,22 +1,28 @@
 package com.kingcontaria.fastquit.mixin;
 
 import com.kingcontaria.fastquit.FastQuit;
+import net.minecraft.client.realms.dto.RealmsWorldOptions;
 import net.minecraft.client.realms.gui.screen.RealmsUploadScreen;
+import net.minecraft.client.realms.util.RealmsUploader;
+import net.minecraft.client.realms.util.UploadProgressTracker;
+import net.minecraft.client.session.Session;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.File;
+import java.nio.file.Path;
 
-@Mixin(RealmsUploadScreen.class)
+@Mixin(RealmsUploader.class)
 public abstract class RealmsUploadScreenMixin {
 
     @Inject(
-            method = "tarGzipArchive",
+            method = "<init>",
             at = @At("HEAD")
     )
-    private void fastquit$waitForSaveOnRealmsUpload(File pathToDirectoryFile, CallbackInfoReturnable<File> cir) {
-        FastQuit.getSavingWorld(pathToDirectoryFile.toPath()).ifPresent(FastQuit::wait);
+    private static void fastquit$waitForSaveOnRealmsUpload(Path directory, RealmsWorldOptions options, Session session, long worldId, int slotId, UploadProgressTracker progressTracker, CallbackInfo ci) {
+        FastQuit.getSavingWorld(directory).ifPresent(FastQuit::wait);
     }
 }
